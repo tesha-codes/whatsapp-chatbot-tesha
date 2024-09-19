a85cc204e49f14368849ed4acf79f2efded1320f
Walter Banda <walterbanda98@gmail.com>
2024-09-20 00:29:42 +0200

    I overcooked

--- Models/User.model.js
+++ Models/User.model.js
@@ -65,13 +65,13 @@ const UserSchema = new mongoose.Schema({
         , "serviceProvider": "text"
     })
 
-UserSchema.plugin(fieldEncryption, {
-    fields: ['phone', 'firstName', 'lastName', 'accountType', 'dob', 'address.physicalAddress', 'address.coordinates', 'profile',],
-    secret: process.env.MONGO_SECRET_ENCRYPTION_KEY,
-    saltGenerator: function (secret) {
-        const salt = crypto.randomBytes(16).toString("hex").slice(0, 16);
-        return salt;
-    }
-})
+// UserSchema.plugin(fieldEncryption, {
+//     fields: ['phone', 'firstName', 'lastName', 'accountType', 'dob', 'address.physicalAddress', 'address.coordinates', 'profile',],
+//     secret: process.env.MONGO_SECRET_ENCRYPTION_KEY,
+//     saltGenerator: function (secret) {
+//         const salt = crypto.randomBytes(16).toString("hex").slice(0, 16);
+//         return salt;
+//     }
+// })
 module.exports = mongoose.model("User", UserSchema);
 
