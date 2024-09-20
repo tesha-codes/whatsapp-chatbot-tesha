27b54ce6d72ce4b50ec32d6bc36fc5fa626e990b
tesha-codes <support@tesha.co.zw>
2024-09-20 17:00:56 +0200

    Delete database directory

--- database/Connect.database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = function (connectionURL) {
-    return mongoose.connect(connectionURL).then((response) => {
-        return response.connection
-    }).catch((error) => {
-        throw error;
-    })
-}
