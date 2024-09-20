83a2f5979a4aa8d6084cbf899d87b55e6db100b4
tesha-codes <support@tesha.co.zw>
2024-09-20 17:01:53 +0200

    Delete routes directory

--- routes/User.routes.js
+++
@@ -1,6 +0,0 @@
-const { registerNewUser } = require("./../Controllers/User.controllers");
-const router = require('express').Router();
-
-router.route("/user/registration").get(registerNewUser)
-
-module.exports = router
