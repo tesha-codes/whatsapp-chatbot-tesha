2398ce4d5fef19a1e31aab71df0536dea6ee8161
TheArtifulProgrammer <theartfulprogrammer@gmail.com>
2024-10-01 13:33:56 +0200

    add common variables in provider

--- modules/provider.js
+++ modules/provider.js
@@ -1,20 +1,37 @@
 const { StatusCodes } = require("http-status-codes");
 
 class ServiceProvider {
-  constructor(res, userResponse, session, user, steps) {
+  constructor(res, userResponse, session, user, steps, messages) {
     this.res = res;
     this.userResponse = userResponse;
     this.session = session;
     this.user = user;
     this.steps = steps;
+    this.messages = messages;
     this.lActivity = Date.now();
+    this.setupCommonVariables();
+  }
+
+  //   setup common variables
+  setupCommonVariables() {
+    const { userResponse } = this;
+    this.phone = userResponse.sender.phone;
+    this.message = userResponse.payload?.text || "";
+    this.username = userResponse.sender.name;
   }
 
   async mainEntry() {
-    const { userResponse, res, session, user, steps } = this;
-    const phone = userResponse.sender.phone;
-    const message = userResponse.payload?.text || "";
-    const username = userResponse.sender.name;
+   const {
+     res,
+     session,
+     user,
+     steps,
+     messages,
+     lActivity,
+     phone,
+     username,
+     message,
+   } = this;
     // : do the dirty work here
     // :
     return res.status(StatusCodes.OK).send("Oh, ndikuda basa here mudhara?");
