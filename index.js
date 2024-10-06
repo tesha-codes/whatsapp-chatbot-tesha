53ea098a3476b6c7c1949d803f3f75c956204c71
Walter Banda <walterbanda98@gmail.com>
2024-10-06 11:56:39 +0200

    index.js file cleaning

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const { queueProviderSearch } = require('./jobs/service-provider.job')
 const {
   clientMainMenuTemplate,
 } = require("./services/whatsappService");
@@ -25,6 +24,7 @@ const ServiceProvider = require("./modules/provider");
 const Onboarding = require("./modules/onboarding");
 const { onGetRequestHandler } = require("./controllers/request.controller");
 const { getRequestedServiceProviders } = require("./controllers/serviceProvider.controller");
+const Client = require("./modules/request-services");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -130,244 +130,18 @@ app.post("/bot", async (req, res) => {
         // : check they user or service provider
         console.log("The sessions here: ", session);
         if (session.accountType === "Client") {
-          // : user
-          //  request service
-          // list services
-          // : acknlowledge request
-          console.log('Session: line 138', session);
-          if (session.step === steps.DEFAULT_CLIENT_STATE) {
-
-            const user = await getUser(phone);
-            await clientMainMenuTemplate(phone, user.firstName)
-            await setSession(phone, {
-              step: steps.SELECT_SERVICE_CATEGORY,
-              message,
-              lActivity,
-            });
-            return res.status(StatusCodes.OK).send(`Hello there 👋 ${user.firstName}`)
-          }
-
-          if (session.step === steps.SETUP_CLIENT_PROFILE) {
-            if (message.toString().toLowerCase() === "create account") {
-              await setSession(phone, {
-                step: steps.COLLECT_USER_FULL_NAME,
-                message,
-                lActivity,
-              });
-              return res.status(StatusCodes.OK).send(messages.GET_FULL_NAME);
-            } else {
-              await setSession(phone, {
-                step: steps.DEFAULT_CLIENT_STATE,
-                message,
-                lActivity,
-              });
-              return res
-                .status(StatusCodes.OK)
-                .send(
-                  "❌ You have cancelled creating profile. You need to have a profile to be able to request services. "
-                );
-            }
-            // NOTE: Collected full name here, now collect national id
-          } else if (session.step === steps.COLLECT_USER_FULL_NAME) {
-            if (message.toString().length < 5) {
-              return res
-                .status(StatusCodes.OK)
-                .send(
-                  "❌ Name and surname provided is too shot.Please re-enter your full name, name(s) first and then surname second."
-                );
-            }
-            const userNames = message.toString().split(" ");
-            const lastName = userNames[userNames.length - 1];
-            const firstName = message.toString().replace(lastName, " ").trim();
-
-            await updateUser({ phone, firstName, lastName });
-            await setSession(phone, {
-              step: steps.COLLECT_USER_ID,
-              message,
-              lActivity,
-            });
-            return res.status(StatusCodes.OK).send(messages.GET_NATIONAL_ID);
-            // NOTE: Collected national id, now collect address
-          } else if (session.step === steps.COLLECT_USER_ID) {
-            const pattern = /^(\d{2})-(\d{7})-([A-Z])-(\d{2})$/;
-            if (!pattern.test(message.toString())) {
-              return res
-                .status(StatusCodes.OK)
-                .send(
-                  "❌ Invalid National Id format , please provide id in the format specified in the example."
-                );
-            }
-
-            const nationalId = message.toString();
-            await updateUser({ phone, nationalId });
-            await setSession(phone, {
-              step: steps.COLLECT_USER_ADDRESS,
-              message,
-              lActivity,
-            });
-            return res.status(StatusCodes.OK).send(messages.GET_ADDRESS);
-            // NOTE: Collected address, sent confirmation and main menu
-          } else if (session.step === steps.COLLECT_USER_ADDRESS) {
-            const street = message.toString();
-            await updateUser({
-              phone,
-              address: {
-                physicalAddress: street,
-              },
-            });
-            await setSession(phone, {
-              step: steps.COLLECT_USER_LOCATION,
-              message,
-              lActivity,
-            });
-
-            return res.status(StatusCodes.OK).send(messages.GET_LOCATION);
-            //
-          }
-          else if (session.step === steps.COLLECT_USER_LOCATION) {
-            console.log('Location here:', message);
-
-            await updateUser({
-              phone,
-              address: {
-                coordinates: message
-              },
-            });
-
-            const confirmation = `
-*Profile Setup Confirmation*
-
-✅ Thank you! Your profile has been successfully set up.
-You’re all set! If you need any further assistance, feel free to reach out. 😊
-`;
-
-            // NOTE:  you can pull the actual name of the client here NOT the whatsapp username used
-            // NOTE: Used SetImmediate in place of setTimeout O Only for testing. I believe it could be efficient or it could be
-            setImmediate(
-              async () => {
-
-                const user = await getUser(phone);
-                console.log(user);
-                await clientMainMenuTemplate(phone, user.firstName)
-                await setSession(phone, {
-                  step: steps.SELECT_SERVICE_CATEGORY,
-                  message,
-                  lActivity,
-                });
-              }
-            );
-            return res.status(StatusCodes.OK).send(confirmation);
-
-          }
-          // NOTE: Received service request
-          else if (
-            session.step === steps.SELECT_SERVICE_CATEGORY &&
-            message.toString().toLowerCase() === "request service"
-          ) {
-            await setSession(phone, {
-              step: steps.SELECT_SERVICE,
-              message,
-              lActivity,
-            });
-
-            return res
-              .status(StatusCodes.OK)
-              .send(messages.CLIENT_WELCOME_MESSAGE);
-          }
-          else if (session.step === steps.SELECT_SERVICE) {
-            const category = await Category.findOne(
-              { code: +message.toLowerCase() },
-              { _id: 1, name: 1 }
-            );
-
-            let queryId = new mongoose.Types.ObjectId(category._id);
-            const services = await Service.find({ category: queryId });
-
-            let responseMessage = `
-
-*${category.name}* 
-Please select a service from the list below:
-${services
-                .map((s, index) => `${index + 1}. *${s.title}*\n${s.description}`)
-                .join("\n\n")}
-
-Reply with the number of the service you'd like to hire.
-            `;
-            await setSession(phone, {
-              step: steps.BOOK_SERVICE,
-              message,
-              lActivity,
-              categoryId: category._id.toString(),
-            });
-            return res.status(StatusCodes.OK).send(responseMessage);
-          }
-
-          else if (session.step === steps.BOOK_SERVICE &&
-            session.categoryId) {
-
-          }
-
-          else if (
-            session.step === steps.BOOK_SERVICE &&
-            session.categoryId
-          ) {
-            const service = await Service.findOne({
-              code: +message,
-              category: session.categoryId,
-            });
-            const user = await User.findOne({ phone });
-            console.log(user);
-
-            const reqID =
-              "REQ" + crypto.randomBytes(3).toString("hex").toUpperCase();
-            const request = await ServiceRequest.create({
-              _id: new mongoose.Types.ObjectId(),
-              city: "Harare",
-              requester: user._id,
-              service: service._id,
-              address: user.address,
-              notes: "Service booking is still in dev",
-              id: reqID,
-            });
-
-            await request.save();
-            const responseMessage = `
-
-📃 Thank you, *${user.username}*! 
-
-Your request for the service  has been successfully created. 
-
-📝 Your request ID is: *${reqID}*. 
-📍 Location: *${request.address.physicalAddress}*
-
-Our team will connect you with a service provider shortly. 
- Please wait...`;
-
-            await queueProviderSearch({
-              phone,
-              serviceId: service._id.toString(),
-              categoryId: session.categoryId,
-              requestId: request._id.toString(),
-            });
-
-
-            setSession(phone, {
-              step: steps.DEFAULT_CLIENT_STATE,
-              message,
-              lActivity,
-              serviceId: service._id.toString(),
-              requestId: request._id.toString(),
-            });
-
-            return res.status(StatusCodes.OK).send(responseMessage)
-          }
-          else if (session.step === steps.SELECT_SERVICE_PROVIDER) {
-            console.log('Service provider value selected', message);
-            // RESPOND TO USER FOR THE SERVICE CREATED
-          }
-
-
           console.log("Client session: ", session);
+
+          const client = new Client(
+            res,
+            userResponse,
+            session,
+            user,
+            steps,
+            messages
+          );
+          return await client.mainEntry()
+
         } else {
           //  MAIN GATE FOR SERVICE PROVIDERS
           const provider = new ServiceProvider(
---
+++ modules/request-services.js
@@ -0,0 +1,269 @@
+
+const { StatusCodes } = require("http-status-codes");
+const { setSession } = require("../utils/redis");
+const { createUser, updateUser ,getUser } = require("../controllers/user.controllers");
+const { formatDateTime } = require("../utils/dateUtil");
+const Category =  require('../models/category.model')
+const { clientMainMenuTemplate } = require('../services/whatsappService')
+const Service = require('../models/services.model')
+const mongoose = require('mongoose')
+const ServiceRequest =  require('../models/request.model')
+const User = require('../models/user.model')
+const crypto = require("node:crypto");
+const { queueProviderSearch } = require('./jobs/service-provider.job')
+
+
+
+class Client {
+    constructor(res, userResponse, session, user, steps, messages) {
+        this.res = res;
+        this.userResponse = userResponse;
+        this.session = session;
+        this.user = user;
+        this.steps = steps;
+        this.messages = messages;
+        this.lActivity = formatDateTime();
+        this.setupCommonVariables();
+    }
+
+    setupCommonVariables() {
+        const { userResponse } = this;
+        this.phone = userResponse.sender.phone;
+        this.message = userResponse.payload?.text || "";
+        this.username = userResponse.sender.name;
+    }
+
+    async mainEntry() {
+        const { session, steps } = this;
+
+        switch (session.step) {
+            case steps.SETUP_CLIENT_PROFILE:
+                return await this.setupClientProfile();
+            case steps.COLLECT_USER_FULL_NAME:
+                return await this.collectFullName();
+            case steps.COLLECT_USER_ID:
+                return await this.collectNationalId();
+            case steps.COLLECT_USER_ADDRESS:
+                return await this.collectAddress();
+            case steps.COLLECT_USER_LOCATION:
+                return await this.collectLocation();
+            case steps.SELECT_SERVICE_CATEGORY:
+                return await this.selectServiceCategory();
+            case steps.SELECT_SERVICE:
+                return await this.selectService();
+            case steps.BOOK_SERVICE:
+                return await this.bookService();
+            default:
+                return await this.handleDefaultState();
+        }
+    }
+
+    async setupClientProfile() {
+        const { res, steps, lActivity, phone, message } = this;
+        if (message.toLowerCase() === "create account") {
+            await setSession(phone, {
+                step: steps.COLLECT_USER_FULL_NAME,
+                message,
+                lActivity,
+            });
+            return res.status(StatusCodes.OK).send(this.messages.GET_FULL_NAME);
+        } else {
+            await setSession(phone, {
+                step: steps.DEFAULT_CLIENT_STATE,
+                message,
+                lActivity,
+            });
+            return res
+                .status(StatusCodes.OK)
+                .send(
+                    "❌ You have cancelled creating profile. You need to have a profile to be able to request services. "
+                );
+        }
+    }
+
+    async collectFullName() {
+        const { res, steps, lActivity, phone, message } = this;
+        if (message.length < 5) {
+            return res
+                .status(StatusCodes.OK)
+                .send(
+                    "❌ Name and surname provided is too short. Please re-enter your full name, name(s) first and then surname second."
+                );
+        }
+        const userNames = message.split(" ");
+        const lastName = userNames[userNames.length - 1];
+        const firstName = message.replace(lastName, " ").trim();
+
+        await updateUser({ phone, firstName, lastName });
+        await setSession(phone, {
+            step: steps.COLLECT_USER_ID,
+            message,
+            lActivity,
+        });
+        return res.status(StatusCodes.OK).send(this.messages.GET_NATIONAL_ID);
+    }
+
+    async collectNationalId() {
+        const { res, steps, lActivity, phone, message } = this;
+        const pattern = /^(\d{2})-(\d{7})-([A-Z])-(\d{2})$/;
+        if (!pattern.test(message)) {
+            return res
+                .status(StatusCodes.OK)
+                .send(
+                    "❌ Invalid National Id format, please provide id in the format specified in the example."
+                );
+        }
+
+        await updateUser({ phone, nationalId: message });
+        await setSession(phone, {
+            step: steps.COLLECT_USER_ADDRESS,
+            message,
+            lActivity,
+        });
+        return res.status(StatusCodes.OK).send(this.messages.GET_ADDRESS);
+    }
+
+    async collectAddress() {
+        const { res, steps, lActivity, phone, message } = this;
+        await updateUser({
+            phone,
+            address: {
+                physicalAddress: message,
+            },
+        });
+        await setSession(phone, {
+            step: steps.COLLECT_USER_LOCATION,
+            message,
+            lActivity,
+        });
+        return res.status(StatusCodes.OK).send(this.messages.GET_LOCATION);
+    }
+
+    async collectLocation() {
+        const { res, steps, lActivity, phone, message } = this;
+        await updateUser({
+            phone,
+            address: {
+                coordinates: message
+            },
+        });
+
+        const confirmation = `
+*Profile Setup Confirmation*
+
+✅ Thank you! Your profile has been successfully set up.
+You're all set! If you need any further assistance, feel free to reach out. 😊
+`;
+
+        setImmediate(async () => {
+            const user = await getUser(phone);
+            await clientMainMenuTemplate(phone, user.firstName);
+            await setSession(phone, {
+                step: steps.SELECT_SERVICE_CATEGORY,
+                message,
+                lActivity,
+            });
+        });
+
+        return res.status(StatusCodes.OK).send(confirmation);
+    }
+
+    async selectServiceCategory() {
+        const { res, steps, lActivity, phone, message } = this;
+        if (message.toLowerCase() === "request service") {
+            await setSession(phone, {
+                step: steps.SELECT_SERVICE,
+                message,
+                lActivity,
+            });
+            return res
+                .status(StatusCodes.OK)
+                .send(this.messages.CLIENT_WELCOME_MESSAGE);
+        }
+        // Handle other cases if needed
+    }
+
+    async selectService() {
+        const { res, steps, lActivity, phone, message } = this;
+        const category = await Category.findOne(
+            { code: +message },
+            { _id: 1, name: 1 }
+        );
+
+        let queryId = new mongoose.Types.ObjectId(category._id);
+        const services = await Service.find({ category: queryId });
+
+        let responseMessage = `
+*${category.name}* 
+Please select a service from the list below:
+${services
+                .map((s, index) => `${index + 1}. *${s.title}*\n${s.description}`)
+                .join("\n\n")}
+
+Reply with the number of the service you'd like to hire.
+    `;
+        await setSession(phone, {
+            step: steps.BOOK_SERVICE,
+            message,
+            lActivity,
+            categoryId: category._id.toString(),
+        });
+        return res.status(StatusCodes.OK).send(responseMessage);
+    }
+
+    async bookService() {
+        const { res, steps, lActivity, phone, message, session } = this;
+        const service = await Service.findOne({
+            code: +message,
+            category: session.categoryId,
+        });
+        const user = await User.findOne({ phone });
+
+        const reqID = "REQ" + crypto.randomBytes(3).toString("hex").toUpperCase();
+        const request = await ServiceRequest.create({
+            _id: new mongoose.Types.ObjectId(),
+            city: "Harare",
+            requester: user._id,
+            service: service._id,
+            address: user.address,
+            notes: "Service booking is still in dev",
+            id: reqID,
+        });
+
+        await request.save();
+        const responseMessage = `
+📃 Thank you, *${user.username}*! 
+
+Your request for the service has been successfully created. 
+
+📝 Your request ID is: *${reqID}*. 
+📍 Location: *${request.address.physicalAddress}*
+
+Our team will connect you with a service provider shortly. 
+Please wait...`;
+
+        await queueProviderSearch({
+            phone,
+            serviceId: service._id.toString(),
+            categoryId: session.categoryId,
+            requestId: request._id.toString(),
+        });
+
+        setSession(phone, {
+            step: steps.DEFAULT_CLIENT_STATE,
+            message,
+            lActivity,
+            serviceId: service._id.toString(),
+            requestId: request._id.toString(),
+        });
+
+        return res.status(StatusCodes.OK).send(responseMessage);
+    }
+
+    async handleDefaultState() {
+        // Handle any default state logic here
+        // This could include showing the main menu or handling unexpected states
+    }
+}
+
+module.exports = Client;
