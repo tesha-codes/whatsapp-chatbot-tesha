ad4ef12b5416aa5a88c710ab429b2918bd6cffb0
TheArtifulProgrammer <theartfulprogrammer@gmail.com>
2025-04-22 19:17:06 +0200

    fix payment webhook

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -61,6 +61,7 @@ router.post("/update", async (req, res) => {
       const provider = await ServiceProvider.findById(
         request.serviceProvider._id
       ).populate("user");
+      
       const newOutstandingPayments = Math.max(
         0,
         (provider.outstandingPayments || 1) - 1
